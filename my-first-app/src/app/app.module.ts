8a9d4d05655e8f70661933362b96e6f0cdeaed50
Sean Swanson <sean-swan@hotmail.com>
2019-12-25 10:47:23 -0800

    Removing components made from assignment to continue with the base project.

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,16 +3,9 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { ServerComponent } from "./server/server.component";
-import { WarningAlertComponent } from "./warning-alert/warning-alert.component";
-import { SuccessAlertComponent } from "./success-alert/success-alert.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
-  ],
+  declarations: [AppComponent, ServerComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
